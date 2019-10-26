8bbe48b06695e1a917cd1c410781e8e8fcb790d8
Denis Karpoukhine <denis.karpoukhine@nordakademie.de>
2019-10-26 11:22:29 +0200

    app module imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {RoomsComponent} from './rooms/rooms.component';
-import {RoomListComponent} from './rooms/room-list/room-list.component';
-import {RoomFormComponent} from './rooms/room-form/room-form.component';
+import {MembersComponent} from './members/members.component';
+import {MemberListComponent} from './members/member-list/member-list.component';
+import {MemberFormComponent} from './members/member-form/member-form.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
     declarations: [
         AppComponent,
-        RoomsComponent,
-        RoomListComponent,
-        RoomFormComponent
+        MembersComponent,
+        MemberListComponent,
+        MemberFormComponent
     ],
     imports: [
         BrowserModule,
